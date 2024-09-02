6b2aeb366ebcdde326275f2c523825b4ca799020
Sofia Gonzalez <miroslava299064@gmail.com>
2024-09-02 07:49:45 -0600

    Added Array function and use of an Object Class

--- app/src/main/java/com/example/myapplication_ejmplo/classes/Variables.kt
+++ app/src/main/java/com/example/myapplication_ejmplo/classes/Variables.kt
@@ -1,6 +1,6 @@
 package com.example.myapplication_ejmplo.classes
 
-class Variables {
+//class Variables {
 
     fun main(){
 
@@ -27,6 +27,23 @@ class Variables {
 
         println(product(10, 92))
 
+        printArray(names)
+
+        println(names.joinToString())
+        
+        val number = arrayOf(1,2,3,4,5,6,7,8,9)
+
+        isEven(number)
+
+        println(getDay(4))
+
+        val person = Person("Andrea", 22)
+
+        println(person.name)
+        println(person.age)
+
+        Person.displayInformation()
+
     }
 
     fun add():Int{
@@ -36,6 +53,45 @@ class Variables {
     }
 
     fun product(x:Int, y:Int):Int{
-        return(x+y)
+        return x+y
     }
-}
+
+    fun printArray(names:Array<String>){
+        for  (name in names){
+            print("Hello $name")
+        }
+    }
+
+    fun isEven(numbers:Array<Int>){
+        for(number in numbers){
+            if(number%2 == 0){
+                println("The number $number is Even")
+            }else{
+                println("The number $number is Odd")
+            }
+        }
+    }
+
+    fun getDay(day:Int): String{
+        var name = ""
+
+        when(day){
+            1 -> name = "Monday"
+            2 -> name = "Tuesday"
+            3 -> name = "Wednesday"
+            4 -> name = "Thursday"
+            5 -> name = "Friday"
+            6 -> name = "Saturday"
+            7 -> name = "Sunday"
+            else -> name = "Incorrect value"
+        }
+        return name
+    }
+
+    class Person (val name: String, val age: Int){
+        fun displayInformation(){
+            println("Name: $name, Age: $age")
+        }
+    }
+
+//}
