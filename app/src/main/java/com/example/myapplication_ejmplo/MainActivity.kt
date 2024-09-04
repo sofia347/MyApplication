183ea3c7c8c5a0282f42b42fee1695c2c6d786d3
Sofia Gonzalez <miroslava299064@gmail.com>
2024-09-04 07:48:16 -0600

    Added layout (columns and row) and added sample modifier

--- app/src/main/java/com/example/myapplication_ejmplo/MainActivity.kt
+++ app/src/main/java/com/example/myapplication_ejmplo/MainActivity.kt
@@ -4,36 +4,70 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication_ejmplo.ui.theme.MyApplication_EjmploTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
-            MyApplication_EjmploTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+            ){
+                Text(text = "Simple Text")
+                ModifierExample()
+                ModifierExample2()
+                ModifierExample3()
             }
-        }
+
+            //Layouts
+            /*Column {
+                Text(text = "Firts Row")
+                Text(text = "Second Row")
+                Text(text = "Third Row")
+                Row {
+                    Text(text = "TEXT 1")
+                    Text(text = "TEXT 2")
+                    Text(text = "TEXT 3")
+                    Text(text = "TEXT 4")
+                }
+                Box {
+                    Text(text = "Label 1")
+                    Text(text = "Label 2")
+                }
+                Greeting(name = "World!")
+                Greeting(name = "World!")
+            }
+        */}
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello HELLO Hello $name!",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
@@ -44,4 +78,52 @@ fun GreetingPreview() {
     MyApplication_EjmploTheme {
         Greeting("Android")
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ModifierExample(){
+    Column(
+        modifier = Modifier.padding(24.dp)
+    ){
+        Text(text = "Hello World")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ModifierExample2(){
+    Column(
+        modifier = Modifier
+            .padding(24.dp)
+            .fillMaxWidth()
+            .clickable(onClick = { clickAction() })
+    ){
+        Text(text = "Hello World")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ModifierExample3(){
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(16.dp)
+            .background(Color.Blue)
+            .border(width = 2.dp, color = Color.Gray)
+            .width(200.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ){
+        Text(text = "Item 1")
+        Text(text = "Item 2")
+        Text(text = "Item 3")
+        Text(text = "Item 4")
+        Text(text = "Item 5")
+    }
+}
+
+fun clickAction(){
+    println("Column Clicked")
 }
