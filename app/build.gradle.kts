d43aa21db49185aa2b39ca1bdf2a2b49038f7fda
Sofia Gonzalez <miroslava299064@gmail.com>
2024-09-24 07:50:29 -0600

    Diseño de interfaz-Correccion de imagenes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.camera.core)
     //implementation(libs.androidx.navigation.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/myapplication_ejmplo/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/myapplication_ejmplo/ui/screens/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication_ejmplo.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
